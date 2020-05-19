1861e2ec264c8b1f9bd6f03e0bd842d077366ea5
Sean Bannigan <bannigan@meetmaestro.com>
2020-05-19 14:36:58 -0400

    adjust formatting

--- android/src/main/java/com/getcapacitor/plugin/http/WebkitCookieManagerProxy.java
+++ android/src/main/java/com/getcapacitor/plugin/http/WebkitCookieManagerProxy.java
@@ -11,86 +11,83 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class WebkitCookieManagerProxy extends CookieManager
-{
-    private android.webkit.CookieManager webkitCookieManager;
+public class WebkitCookieManagerProxy extends CookieManager {
+  private android.webkit.CookieManager webkitCookieManager;
 
-    public String getCookie(String url) {
-        return this.webkitCookieManager.getCookie(url);
+  public String getCookie(String url) {
+    return this.webkitCookieManager.getCookie(url);
+  }
+
+  public void setCookie(String url, String value) {
+    this.webkitCookieManager.setCookie(url, value);
+    this.webkitCookieManager.flush();
+  }
+
+  public void flush() {
+    this.webkitCookieManager.flush();
+  }
+
+  public void removeAllCookies(ValueCallback<Boolean> callback) {
+    this.webkitCookieManager.removeAllCookies(callback);
+  }
+
+  public WebkitCookieManagerProxy() {
+    this(null, null);
+  }
+
+  public WebkitCookieManagerProxy(CookieStore store, CookiePolicy cookiePolicy) {
+    super(null, cookiePolicy);
+
+    this.webkitCookieManager = android.webkit.CookieManager.getInstance();
+  }
+
+  @Override
+  public void put(URI uri, Map<String, List<String>> responseHeaders) throws IOException {
+    // make sure our args are valid
+    if ((uri == null) || (responseHeaders == null))
+      return;
+
+    // save our url once
+    String url = uri.toString();
+
+    // go over the headers
+    for (String headerKey : responseHeaders.keySet()) {
+      // ignore headers which aren't cookie related
+      if ((headerKey == null)
+          || !(headerKey.equalsIgnoreCase("Set-Cookie2") || headerKey.equalsIgnoreCase("Set-Cookie")))
+        continue;
+
+      // process each of the headers
+      for (String headerValue : responseHeaders.get(headerKey)) {
+        this.setCookie(url, headerValue);
+      }
     }
+  }
 
-    public void setCookie(String url, String value) {
-        this.webkitCookieManager.setCookie(url, value);
-        this.webkitCookieManager.flush();
-    }
+  @Override
+  public Map<String, List<String>> get(URI uri, Map<String, List<String>> requestHeaders) throws IOException {
+    // make sure our args are valid
+    if ((uri == null) || (requestHeaders == null))
+      throw new IllegalArgumentException("Argument is null");
 
-    public void flush() {
-        this.webkitCookieManager.flush();
-    }
+    // save our url once
+    String url = uri.toString();
 
-    public void removeAllCookies(ValueCallback<Boolean> callback) {
-        this.webkitCookieManager.removeAllCookies(callback);
-    }
+    // prepare our response
+    Map<String, List<String>> res = new java.util.HashMap<String, List<String>>();
 
-    public WebkitCookieManagerProxy()
-    {
-        this(null, null);
-    }
+    // get the cookie
+    String cookie = this.getCookie(url);
 
-    public WebkitCookieManagerProxy(CookieStore store, CookiePolicy cookiePolicy)
-    {
-        super(null, cookiePolicy);
+    // return it
+    if (cookie != null)
+      res.put("Cookie", Arrays.asList(cookie));
+    return res;
+  }
 
-        this.webkitCookieManager = android.webkit.CookieManager.getInstance();
-    }
-
-    @Override
-    public void put(URI uri, Map<String, List<String>> responseHeaders) throws IOException
-    {
-        // make sure our args are valid
-        if ((uri == null) || (responseHeaders == null)) return;
-
-        // save our url once
-        String url = uri.toString();
-
-        // go over the headers
-        for (String headerKey : responseHeaders.keySet())
-        {
-            // ignore headers which aren't cookie related
-            if ((headerKey == null) || !(headerKey.equalsIgnoreCase("Set-Cookie2") || headerKey.equalsIgnoreCase("Set-Cookie"))) continue;
-
-            // process each of the headers
-            for (String headerValue : responseHeaders.get(headerKey))
-            {
-                this.setCookie(url, headerValue);
-            }
-        }
-    }
-
-    @Override
-    public Map<String, List<String>> get(URI uri, Map<String, List<String>> requestHeaders) throws IOException
-    {
-        // make sure our args are valid
-        if ((uri == null) || (requestHeaders == null)) throw new IllegalArgumentException("Argument is null");
-
-        // save our url once
-        String url = uri.toString();
-
-        // prepare our response
-        Map<String, List<String>> res = new java.util.HashMap<String, List<String>>();
-
-        // get the cookie
-        String cookie = this.getCookie(url);
-
-        // return it
-        if (cookie != null) res.put("Cookie", Arrays.asList(cookie));
-        return res;
-    }
-
-    @Override
-    public CookieStore getCookieStore()
-    {
-        // we don't want anyone to work with this cookie store directly
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public CookieStore getCookieStore() {
+    // we don't want anyone to work with this cookie store directly
+    throw new UnsupportedOperationException();
+  }
 }
