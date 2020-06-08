46ef183db9acdf3cf1c1871f2bc49f2674961af2
Andy Sousa <andy.sousa@carrier.com>
2020-06-08 11:42:17 -0400

    added data object to non json content type web calls

--- dist/esm/web.js
+++ dist/esm/web.js
@@ -38,8 +38,10 @@ export class HttpPluginWeb extends WebPlugin {
             req['body'] = JSON.stringify(options.data);
         }
         else if (contentType.indexOf('application/x-www-form-urlencoded') === 0) {
+            req['body'] = options.data[Object.keys(options.data)[0]];
         }
         else if (contentType.indexOf('multipart/form-data') === 0) {
+            req['body'] = options.data[Object.keys(options.data)[0]];
         }
         return req;
     }
