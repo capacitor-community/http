8fc8b5c0fbf6bfc0687d149468cc22081b44e446
Andy Sousa <andy.sousa@carrier.com>
2020-06-08 11:54:13 -0400

    fixed the mess from not having latest

--- src/web.ts
+++ src/web.ts
@@ -58,9 +58,10 @@ export class HttpPluginWeb extends WebPlugin implements HttpPlugin {
     if (contentType.indexOf('application/json') === 0) {
       req['body'] = JSON.stringify(options.data);
     } else if (contentType.indexOf('application/x-www-form-urlencoded') === 0) {
+        req['body'] = options.data[Object.keys(options.data)[0]];
     } else if (contentType.indexOf('multipart/form-data') === 0) {
+        req['body'] = options.data[Object.keys(options.data)[0]];
     }
-
     return req;
   }
 
