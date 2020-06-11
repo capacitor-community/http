00731e73b24ad4c0bfbca5dd24e8c18efc17f885
Andy Sousa <andy.sousa@carrier.com>
2020-06-10 21:40:45 -0400

    reverted includes on content type

--- src/web.ts
+++ src/web.ts
@@ -55,10 +55,10 @@ export class HttpPluginWeb extends WebPlugin implements HttpPlugin {
 
     const contentType = this.getRequestHeader(options.headers || {}, 'content-type') || '';
 
-    if (contentType.includes('application/json')) {
+    if (contentType.indexOf('application/json') === 0) {
       req['body'] = JSON.stringify(options.data);
-    } else if ((contentType.includes('application/x-www-form-urlencoded')) ||
-              (contentType.includes('multipart/form-data'))) {
+    } else if ((contentType.indexOf('application/x-www-form-urlencoded') === 0) ||
+              (contentType.indexOf('multipart/form-data') === 0)) {
       const urlSearchParams = new URLSearchParams();
       for (let key of Object.keys(options.data)) {
           urlSearchParams.set(key, options.data[key]);
