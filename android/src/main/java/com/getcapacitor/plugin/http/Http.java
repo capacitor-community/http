0ace2c088fa86031219ae15ac7144e53cb06ab6d
Lucas Silva <lucashenrique125@gmail.com>
2020-10-27 18:34:08 -0300

    Add missing final statement

--- android/src/main/java/com/getcapacitor/plugin/http/Http.java
+++ android/src/main/java/com/getcapacitor/plugin/http/Http.java
@@ -46,28 +46,32 @@ public class Http extends Plugin {
     }
 
     @PluginMethod
-    public void request(PluginCall call) {
-        new Thread(new Runnable() {
-            public void run() {
-                String url = call.getString("url");
-                String method = call.getString("method");
-                JSObject headers = call.getObject("headers");
-                JSObject params = call.getObject("params");
-        
-                switch (method) {
-                case "GET":
-                case "HEAD":
-                    get(call, url, method, headers, params);
-                    return;
-                case "DELETE":
-                case "PATCH":
-                case "POST":
-                case "PUT":
-                    mutate(call, url, method, headers);
-                    return;
+    public void request(final PluginCall call) {
+        new Thread(
+            new Runnable() {
+
+                public void run() {
+                    String url = call.getString("url");
+                    String method = call.getString("method");
+                    JSObject headers = call.getObject("headers");
+                    JSObject params = call.getObject("params");
+
+                    switch (method) {
+                        case "GET":
+                        case "HEAD":
+                            get(call, url, method, headers, params);
+                            return;
+                        case "DELETE":
+                        case "PATCH":
+                        case "POST":
+                        case "PUT":
+                            mutate(call, url, method, headers);
+                            return;
+                    }
                 }
             }
-        }).start();
+        )
+        .start();
     }
 
     private void get(PluginCall call, String urlString, String method, JSObject headers, JSObject params) {
