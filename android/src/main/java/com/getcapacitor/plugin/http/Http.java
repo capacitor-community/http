5a0730a9b076e0198b567e5be80819994e9966e1
Hector Cruz <mghcs87@gmail.com>
2021-01-16 19:10:53 -0600

    Fix (Android) add line break in GET request data

--- android/src/main/java/com/getcapacitor/plugin/http/Http.java
+++ android/src/main/java/com/getcapacitor/plugin/http/Http.java
@@ -391,7 +391,7 @@ public class Http extends Plugin {
         StringBuilder builder = new StringBuilder();
         String line;
         while ((line = in.readLine()) != null) {
-            builder.append(line);
+            builder.append(line).append(System.getProperty("line.separator"));
         }
         in.close();
 
