1ffb34b748c309d106aa35ba57db9e884e97c4bd
Thomas Vidas <hello@thomasvidas.com>
2021-03-24 13:52:19 -0400

    ts-ignore prettier fix

--- src/web.ts
+++ src/web.ts
@@ -121,8 +121,8 @@ export class HttpWeb extends WebPlugin implements HttpPlugin {
   /**
    * Clears out cookies by setting them to expire immediately
    */
-  // @ts-ignore
   public clearCookies = async (
+    // @ts-ignore
     options: HttpMultiCookiesOptions,
   ): Promise<void> => Cookie.clearCookies();
 
