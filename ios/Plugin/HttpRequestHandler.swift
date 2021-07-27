af05612a4369fdbce61b9295565cda07b56f2d82
Joe Flateau <joe@joeflateau.net>
2021-07-27 10:55:00 -0400

    fix(iOS): pass http method to request functions on ios (#152)

--- ios/Plugin/HttpRequestHandler.swift
+++ ios/Plugin/HttpRequestHandler.swift
@@ -150,9 +150,9 @@ class HttpRequestHandler {
     }
 
 
-    public static func request(_ call: CAPPluginCall) throws {
+    public static func request(_ call: CAPPluginCall, _ httpMethod: String?) throws {
         guard let urlString = call.getString("url") else { throw URLError(.badURL) }
-        guard let method = call.getString("method") else { throw URLError(.dataNotAllowed) }
+        guard let method = httpMethod ?? call.getString("method") else { throw URLError(.dataNotAllowed) }
 
         let headers = (call.getObject("headers") ?? [:]) as! [String: String]
         let params = (call.getObject("params") ?? [:]) as! [String: Any]
