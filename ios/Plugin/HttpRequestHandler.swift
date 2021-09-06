2373bbcaad24da359054a3e91b5d9ca80bd82b37
Steven Fluck <4992472+7-plus-t@users.noreply.github.com>
2021-09-06 16:23:45 +0200

    Remove temporary work-around for ignoring certificate checks

--- ios/Plugin/HttpRequestHandler.swift
+++ ios/Plugin/HttpRequestHandler.swift
@@ -39,9 +39,6 @@ func tryParseJson(_ data: Data) -> Any {
 }
 
 class HttpRequestHandler {
-    
-    public static var allowAllDelegate = AllowAllUrlSessionDelegate()
-    
     private class CapacitorHttpRequestBuilder {
         private var url: URL?
         private var method: String?
@@ -190,10 +187,7 @@ class HttpRequestHandler {
         }
 
         let urlRequest = request.getUrlRequest()
-        
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: allowAllDelegate, delegateQueue: nil)
-        
-        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             if error != nil {
                 call.reject("Error", "REQUEST", error, [:])
                 return
@@ -240,10 +234,7 @@ class HttpRequestHandler {
         guard let form = try? generateMultipartForm(fileUrl, name, boundary, body) else { throw URLError(.cannotCreateFile) }
 
         let urlRequest = request.getUrlRequest()
-        
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: allowAllDelegate, delegateQueue: nil)
-        
-        let task = session.uploadTask(with: urlRequest, from: form) { (data, response, error) in
+        let task = URLSession.shared.uploadTask(with: urlRequest, from: form) { (data, response, error) in
             if error != nil {
                 CAPLog.print("Error on upload file", String(describing: data), String(describing: response), String(describing: error))
                 call.reject("Error", "UPLOAD", error, [:])
@@ -281,10 +272,7 @@ class HttpRequestHandler {
         request.setTimeout(timeout)
 
         let urlRequest = request.getUrlRequest()
-        
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: allowAllDelegate, delegateQueue: nil)
-        
-        let task = session.downloadTask(with: urlRequest) { (downloadLocation, response, error) in
+        let task = URLSession.shared.downloadTask(with: urlRequest) { (downloadLocation, response, error) in
             if error != nil {
                 CAPLog.print("Error on download file", String(describing: downloadLocation), String(describing: response), String(describing: error))
                 call.reject("Error", "DOWNLOAD", error, [:])
@@ -320,16 +308,4 @@ class HttpRequestHandler {
 
         task.resume()
     }
-}
-
-/// Temporary helper class, to allow connections to all URLs, even with selfsigned certificates.
-/// This class must later on be replaced with a config sensitive one, which respects the settings in the plist files.
-class AllowAllUrlSessionDelegate: NSObject, URLSessionDelegate {
-    
-    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-           //Trust the certificate even if not valid
-           let urlCredential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
-
-           completionHandler(.useCredential, urlCredential)
-        }
 }
